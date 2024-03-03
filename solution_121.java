ab957636d507d162c905e601d0d76ec8d4f3b1bf
Hao Zhou <tristanchow.1999@gmail.com>
2024-03-03 15:41:10 -0600

    better ans for 121

--- solution_121.java
+++ solution_121.java
@@ -1,29 +1,45 @@
 import java.util.Arrays;
-class Solution {
-    public int maxProfit(int[] prices) {
-        int n = prices.length;
-        if (n == 1) return 0;
-        else{
-            int[] left = Arrays.copyOfRange(prices, 0, n/2);
-            int[] right = Arrays.copyOfRange(prices, n/2, n);
-            int lmin = findMin(left);
-            int rmax = findMax(right);
-            int pfc = rmax - lmin > 0 ? rmax -lmin : 0;
-            return Math.max(Math.max(pfc, maxProfit(right)), maxProfit(left));
-        }
+
+class solution_121 {
+  public int maxProfit_b(int[] prices) {
+    int prof = 0;
+    int min = Integer.MAX_VALUE;
+    for (int i = 0; i < prices.length; i++) {
+      if(prices[i]< min) min = prices[i];
+      prof = prices[i]-min > prof ? prices[i]-min : prof;
     }
-    public int findMax(int[] a){
-        int m = a[0];
-        for (int i = 0; i < a.length; i++){
-            if (a[i] > m) m = a[i];
-        }
-        return m;
+    return prof;
+  }
+
+  public int maxProfit(int[] prices) {
+    int n = prices.length;
+    if (n == 1)
+      return 0;
+    else {
+      int[] left = Arrays.copyOfRange(prices, 0, n / 2);
+      int[] right = Arrays.copyOfRange(prices, n / 2, n);
+      int lmin = findMin(left);
+      int rmax = findMax(right);
+      int pfc = rmax - lmin > 0 ? rmax - lmin : 0;
+      return Math.max(Math.max(pfc, maxProfit(right)), maxProfit(left));
     }
-    public int findMin(int[] a){
-        int m = a[0];
-        for (int i = 0; i < a.length; i++){
-            if (a[i] < m) m = a[i];
-        }
-        return m;
+  }
+
+  public int findMax(int[] a) {
+    int m = a[0];
+    for (int i = 0; i < a.length; i++) {
+      if (a[i] > m)
+        m = a[i];
     }
+    return m;
+  }
+
+  public int findMin(int[] a) {
+    int m = a[0];
+    for (int i = 0; i < a.length; i++) {
+      if (a[i] < m)
+        m = a[i];
+    }
+    return m;
+  }
 }
