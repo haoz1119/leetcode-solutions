763387309dbab5f7fd2b244590da5449726d0093
haozhou1919 <68983578+haozhou1919@users.noreply.github.com>
2024-03-05 21:24:21 -0600

    169 better

--- solution_169.java
+++ solution_169.java
@@ -14,4 +14,30 @@ class solution_169 {
         return majority;
 
     }
+    /**
+     * Moore's Voting Algorithm
+     * pick the first element as the candidate and increment its count by one, if next element is
+     * the same as the candidate we increment the count, otherwise we decrement the count. when the 
+     * count is 0, we pick the next element as the candidate
+     * 
+     * correctness discussion:
+     * the majority will have a net positive count to itself since it appears more than n/2 times
+     */
+    public int majorityElement_Better(int[] nums) {
+      int count = 0;
+      int element = 0;
 
+      for (int num : nums) {
+          if (count == 0) {
+              element = num;
+              count = 1;
+          } else if (element == num) {
+              count++;
+          } else {
+              count--;
+          }
+      }
+
+      return element;
+    }
+}
