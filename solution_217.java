8a48e39dc3ea0694f04ae3333da2ba23f5247919
Patron <Patron@LMBA.local>
2024-03-06 14:02:04 -0600

    217 improved

--- solution_217.java
+++ solution_217.java
@@ -12,4 +12,26 @@ class solution_217 {
         }
         return flag;
     }
+    // possible better: becaue only one for loop
+    public boolean containsDuplicate_2(int[] nums) {
+      HashMap<Integer, Integer> seen = new HashMap<>();
+      for (int num : nums) {
+          if (seen.containsKey(num) && seen.get(num) >= 1)
+              return true;
+          // getOrDefault is either the value to the key num or 0
+          seen.put(num, seen.getOrDefault(num, 0) + 1);
+      }
+      return false;
+  }
+  // quickest 
+  public boolean containsDuplicate_3(int[] nums) {
+    Set<Integer> set = new HashSet<>();
+
+    for(int num : nums) {
+        if(!set.add(num)) {
+            return true;
+        }
+    }
+    return false;
+}
 }
