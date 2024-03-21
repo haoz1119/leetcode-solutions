5fa8e4bebcac80d91d86b1d1293ac169565f054e
Patron <Patron@LMBA.local>
2024-03-21 18:26:01 -0500

    704

---
+++ solution_704.java
@@ -0,0 +1,15 @@
+class solution_704 {
+  public int search(int[] nums, int target) {
+      int n = nums.length;
+      int l = 0;
+      int r = n - 1;
+      int mid;
+      while(l <= r){
+          mid = (l+r)/2;
+          if(nums[mid] == target) return mid;
+          if(nums[mid] > target) r = mid - 1;
+          if(nums[mid] < target) l = mid + 1;
+      }
+      return -1;
+  }
+}
