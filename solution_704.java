8fffc7fc3dbba96dcfdffe8f09c6cb2d9ad0cc38
Patron <Patron@LMBA.local>
2024-03-21 18:29:15 -0500

    704

--- solution_704.java
+++ solution_704.java
@@ -4,6 +4,7 @@ class solution_704 {
       int l = 0;
       int r = n - 1;
       int mid;
+      // closed interval, thus here we use <= since there can be only one integer in the array
       while(l <= r){
           mid = (l+r)/2;
           if(nums[mid] == target) return mid;
