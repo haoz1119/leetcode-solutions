7e87859fce3bd5b82a012e3d3c7b67e85005d397
haozhou1919 <68983578+haozhou1919@users.noreply.github.com>
2024-03-22 00:11:38 -0500

    59

---
+++ 59.spiral-matrix-ii.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=59 lang=java
+ *
+ * [59] Spiral Matrix II
+ */
+
+// @lc code=start
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int l, r, t, b;
+        l = t = 0;
+        r = b = n - 1;
+        int val = 1;
+        int i, j;
+        i = j = 0;
+        while (l < n) {
+            // fill every val in top row
+            for (j = l; j <= r; j++) {
+                matrix[i][j] = val++;
+            }
+            j--;
+            t++;
+            // fill every val in right col
+            for (i = t; i <= b; i++) {
+                matrix[i][j] = val++;
+            }
+            i--;
+            r--;
+            // fill every val in bottom row(reverse order)
+            for (j = r; j >= l; j--) {
+                matrix[i][j]= val++;
+            }
+            j++;
+            b--;
+            // fill every val in left col(reverse order)
+            for(i=b; i >= t; i--){
+                matrix[i][j]=val++;
+            }
+            i++;
+            l++;
+        }
+        return matrix;
+    }
+}
+// @lc code=end
