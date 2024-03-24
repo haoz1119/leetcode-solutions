e096f86ea44b6f6f9beb47998b2b5e0801fab268
haozhou <Patron@LMBA.attlocal.net>
2024-03-24 15:03:52 -0500

    88

--- 88.merge-sorted-array.java
+++ 88.merge-sorted-array.java
@@ -30,5 +30,22 @@ class Solution {
     
     }
 }
+//better solution, compare and copy from the end of two arrays, as we are not makeing
+// another array
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int l = m - 1;
+        int r = n - 1;
+        for(int i = 0; i < m+n; i++){
+            if(l < 0) {
+                nums1[m+n-1-i]=nums2[r--];
+            }else if(r<0) return;
+            else if(nums1[l] > nums2[r]) nums1[m+n-1-i]= nums1[l--];
+            else{
+                nums1[m+n-1-i]=nums2[r--];
+            }
+        }
+    }
+}
 // @lc code=end
 
