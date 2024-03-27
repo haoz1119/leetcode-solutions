11e40a8af135d03c626160de9073d1cf8050f08e
haozhou <tristanchow.1999@gmail.com>
2024-03-27 18:55:37 -0500

    150

--- 152.maximum-product-subarray.java
+++ 152.maximum-product-subarray.java
@@ -7,7 +7,12 @@
 // @lc code=start
 /**
  * if the array does not have 0, then the maximum product must start from the leftmost element or the rightmost element
+ * 1. if the array has one negative, then the maximum product must start from the leftmost zero or the rightmost zero
+ * if the array has two negatives, then the maximum product is the entire array
+ * if the array has odd number of negatives, 1 would still apply
+ * if the array has even number of negatives, then the maximum product is the entire array
  * if the array does have zeros, then we can split the array in to several subarrays without zeros, That's to say, when the prefix product is 0, we start over and compute prefix product from the current element instead. 
+ * zero acts like a divider.
  */
 class Solution {
     public int maxProduct(int[] A) {
