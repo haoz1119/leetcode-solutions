ef2bf49a3ba68e8b0721ac514f85bf2049b4fbd7
haozhou1919 <68983578+haozhou1919@users.noreply.github.com>
2024-03-31 17:26:37 -0500

    234

---
+++ 234.palindrome-linked-list.java
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode id=234 lang=java
+ *
+ * [234] Palindrome Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        int n = 0;
+        ListNode cur = head;
+        while(cur != null){
+            cur= cur.next;
+            n++;
+        }
+        if(n==1) return true;
+        int mid = n/2;
+        ListNode prev = null;
+        cur = head;
+        ListNode next = null;
+        int i = 0;
+        while(cur != null){
+            if(i >= n/2){
+                next = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = next;
+                i++;
+            }else{
+                prev = cur;
+                cur = cur.next;
+                i++;
+            }
+        }
+        cur = head;
+        for(int j = 0; j < n/2; j++){
+            if(cur.val != prev.val) return false;
+            cur = cur.next;
+            prev = prev.next;
+        }
+        return true;
+        
+        
+    }
+}
+// @lc code=end
+
