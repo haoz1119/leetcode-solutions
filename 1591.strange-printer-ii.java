abb25805be97b8bc511aee8a8dcbf45bc1a7e0eb
haozhou1919 <68983578+haozhou1919@users.noreply.github.com>
2024-03-31 22:57:54 -0500

    159

---
+++ 1591.strange-printer-ii.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1591 lang=java
+ *
+ * [1591] Strange Printer II
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPrintable(int[][] targetGrid) {
+        
+    }
+}
+// @lc code=end
+
