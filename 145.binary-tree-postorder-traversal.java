24d14be5be4458ddf655b9a547b305f794facc4b
haozhou1919 <68983578+haozhou1919@users.noreply.github.com>
2024-04-01 21:55:02 -0500

    145

---
+++ 145.binary-tree-postorder-traversal.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=145 lang=java
+ *
+ * [145] Binary Tree Postorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        ArrayList<Integer> ls = new ArrayList<>();
+        if(root == null) return ls;
+        ls.addAll(postorderTraversal(root.left));
+        ls.addAll(postorderTraversal(root.right));
+        ls.add(root.val);
+        return ls;
+    }
+}
+// @lc code=end
+
