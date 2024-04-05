6d84261d59a278f19543c4e468e49b9e64f17e90
haozhou1919 <68983578+haozhou1919@users.noreply.github.com>
2024-04-04 21:48:44 -0500

    111

--- 111.minimum-depth-of-binary-tree.java
+++ 111.minimum-depth-of-binary-tree.java
@@ -23,22 +23,15 @@
 class Solution {
     public int minDepth(TreeNode root) {
         if(root == null) return 0;
-        int minleft = 0, minright = 0;
-        if(root.left == null && root.right == null){
-            return 1 + Math.min(minDepth(root.left), minDepth(root.right));
-        }
-        else if(root.left != null && root.right == null)  {
-            minleft = minDepth(root.left);
-            return 1 + minleft;
+        if(root.left != null && root.right == null)  {
+            return 1 + minDepth(root.left);
         }
         else if(root.right!=null && root.left == null) {
-            minright = minDepth(root.right);
-            return 1 + minright;
+            return 1 + minDepth(root.right);
         }
         else{
             return 1 + Math.min(minDepth(root.left), minDepth(root.right));
         }
-        
     }
 }
 // @lc code=end
