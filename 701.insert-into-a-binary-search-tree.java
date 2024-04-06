7a1f2fe2dd335fa979c557a682916c63e722be62
haozhou <tristanchow.1999@gmail.com>
2024-04-06 15:29:36 -0500

    701

--- 701.insert-into-a-binary-search-tree.java
+++ 701.insert-into-a-binary-search-tree.java
@@ -36,5 +36,22 @@ class Solution {
         return root;
     }
 }
+
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null) {
+            return new TreeNode(val);
+        }
+        if(root.val < val) {
+            if(root.right == null) root.right = new TreeNode(val);
+            else insertIntoBST(root.right, val);
+        }
+        if(root.val > val){
+            if(root.left == null) root.left = new TreeNode(val);
+            else insertIntoBST(root.left, val);
+        } 
+        return root;
+    }
+}
 // @lc code=end
 
