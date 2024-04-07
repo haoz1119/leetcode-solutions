e11230c8f6f3273751846e536d56d141d31d8830
haozhou <tristanchow.1999@gmail.com>
2024-04-07 18:47:48 -0500

    1448

--- 1448.count-good-nodes-in-binary-tree.java
+++ 1448.count-good-nodes-in-binary-tree.java
@@ -50,5 +50,106 @@ class Solution {
         return count;
     }
 }
+
+// quicker recursive solution
+class Solution {
+    private int numGoodNodes = 0;
+    
+    public int goodNodes(TreeNode root) {
+        dfs(root, Integer.MIN_VALUE);
+        return numGoodNodes;
+    }
+    
+    private void dfs(TreeNode node, int maxSoFar) {
+        if (maxSoFar <= node.val) {
+            numGoodNodes++;
+        }
+        
+        if (node.right != null) {
+            dfs(node.right, Math.max(node.val, maxSoFar));
+        }
+
+        if (node.left != null) {
+            dfs(node.left, Math.max(node.val, maxSoFar));
+        }
+    }
+}
+
+
+// quicker iterative solution,
+// using a stack to simulate the recursive call stack
+// we can use two stacks instead of a Pair class
+class Pair {
+    public TreeNode node;
+    public int maxSoFar;
+    
+    public Pair(TreeNode node, int maxSoFar) {
+        this.node = node;
+        this.maxSoFar = maxSoFar;
+    }
+}
+
+class Solution {
+    public int goodNodes(TreeNode root) {
+        int numGoodNodes = 0;
+        Stack<Pair> stack = new Stack<>();
+        stack.push(new Pair(root, Integer.MIN_VALUE));
+        
+        while (stack.size() > 0) {
+            Pair curr = stack.pop();
+            if (curr.maxSoFar <= curr.node.val) {
+                numGoodNodes++;
+            }
+            
+            if (curr.node.left != null) {
+                stack.push(new Pair(curr.node.left, Math.max(curr.node.val, curr.maxSoFar)));
+            }
+            
+            if (curr.node.right != null) {
+                stack.push(new Pair(curr.node.right, Math.max(curr.node.val, curr.maxSoFar)));
+            }
+        }
+
+        return numGoodNodes;
+    }
+}
+
+// BFS solution
+class Pair {
+    public TreeNode node;
+    public int maxSoFar;
+    
+    public Pair(TreeNode node, int maxSoFar) {
+        this.node = node;
+        this.maxSoFar = maxSoFar;
+    }
+}
+
+class Solution {
+    public int goodNodes(TreeNode root) {
+        int numGoodNodes = 0;
+        Queue<Pair> queue = new LinkedList<>();
+        queue.offer(new Pair(root, Integer.MIN_VALUE));
+        
+        while (queue.size() > 0) {
+            Pair curr = queue.poll();
+            if (curr.maxSoFar <= curr.node.val) {
+                numGoodNodes++;
+            }
+            
+            if (curr.node.right != null) {
+                queue.offer(new Pair(curr.node.right, Math.max(curr.node.val, curr.maxSoFar)));
+            }
+
+            if (curr.node.left != null) {
+                queue.offer(new Pair(curr.node.left, Math.max(curr.node.val, curr.maxSoFar)));
+            }
+        }
+
+        return numGoodNodes;
+    }
+}
+
+
 // @lc code=end
 
