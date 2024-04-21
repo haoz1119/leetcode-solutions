e1f8ffc0a392e81b58fac112abe90fba2b03a088
haozhou <tristanchow.1999@gmail.com>
2024-04-21 16:41:14 -0500

    1971

--- 1971.find-if-path-exists-in-graph.java
+++ 1971.find-if-path-exists-in-graph.java
@@ -80,5 +80,32 @@ class Solution {
         return false;
     }
 }
+
+// quicker solution with union find
+class Solution {
+    int[] uf;
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+       uf = new int[n];
+       for(int i =0;i<n;i++)
+            uf[i] = i;
+
+        for(int[] e : edges){
+            int p1 = find(e[0]);
+            int p2 = find(e[1]);
+            if(p1 != p2)
+                uf[p1] = p2;//union
+        }
+
+        return find(source) == find(destination);
+
+    }
+    private int find(int a){
+        while(uf[a] != a){
+            uf[a] = uf[uf[a]];//compression
+            a = uf[a];
+        }
+        return a;
+    }
+}
 // @lc code=end
 
