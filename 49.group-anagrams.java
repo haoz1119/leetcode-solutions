df5671245ebd17e4bb7e4a610590234ce5a09f17
haozhou1919 <68983578+haozhou1919@users.noreply.github.com>
2024-07-06 17:45:06 -0500

    created solution to 49

--- 49.group-anagrams.java
+++ 49.group-anagrams.java
@@ -60,5 +60,48 @@ class Solution {
         }
     }
 }
+
+// a better solution
+// O(nklogk)
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs.length == 0) return new LinkedList<List<String>>();
+        LinkedList<List<String>> lls = new LinkedList<>();
+        HashMap<String, List<String>> hm = new HashMap<String, List<String>>();
+        for(String st : strs){ // O(n)
+            char[] ch = st.toCharArray();
+            Arrays.sort(ch); // O(klogk)
+            String ss = new String(ch);
+            if(hm.containsKey(ss)){
+                hm.get(ss).add(st);
+            }
+            else{
+                LinkedList<String> ls = new LinkedList<>();
+                lls.add(ls);
+                ls.add(st);
+                hm.put(ss, ls);
+            }
+        }
+        return lls;
+    }
+}
+
+// shortest solution
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // Raw Types: When you create an ArrayList without specifying a generic type, you are using a raw type.
+    // Using raw types bypasses the type safety provided by generics. This means you can add any type of object to the list, and the compiler won't check if the types are correct.
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
+    }
+}
 // @lc code=end
 
