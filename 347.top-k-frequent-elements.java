7d0566fdbfea010023f6792fc3ad87b423d6f6f2
haozhou1919 <68983578+haozhou1919@users.noreply.github.com>
2024-07-07 15:23:01 -0500

    revisited 347 with priority queue

--- 347.top-k-frequent-elements.java
+++ 347.top-k-frequent-elements.java
@@ -7,31 +7,28 @@
 // @lc code=start
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        
-        HashMap <Integer,Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++)
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-
-        ArrayList <Integer> [] list = new ArrayList [nums.length+1];
-        for(int key : map.keySet()){
-            int freq = map.get(key);
-            if(list[freq] == null){
-                list[freq] = new ArrayList<Integer>();
-            }
-            list[freq].add(key);
+        HashMap<Integer, Integer> count = new HashMap<>();
+        for(int n : nums){
+            count.put(n, count.getOrDefault(n, 0)+1);
         }
-
-        int [] res = new int[k];
-
-        for(int i = list.length-1; k > 0; i--){//O(n)
-            if(list[i] == null) continue;
-            for(int key : list[i]){
-                res[--k] = key;
+        // array of arraylist of integers
+        ArrayList<Integer>[] al = new ArrayList[nums.length+1]; // because the biggest count will be nums.length
+        for(int key : count.keySet()){
+            int freq = count.get(key);
+            if(al[freq] == null) al[freq] = new ArrayList<Integer>();
+            al[freq].add(key);
+        }
+        int[] result = new int[k];
+        for(int i = nums.length; k>0; i--){
+            if(al[i]!=null){
+                for(int n : al[i]){
+                    if(k > 0) // guard against the case when the list has more than k elements
+                        result[--k] = n;
+                }
             }
         }
-
-        return res;
-  }  
+        return result;
+    }
 }
 
 // an solution using priority queue
