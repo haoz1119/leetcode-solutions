a40af9af9eb80fffd4b4b6de3ee6de03d99f5aee
haozhou1919 <68983578+haozhou1919@users.noreply.github.com>
2024-07-08 16:56:48 -0500

    updated solution 80

--- 80.remove-duplicates-from-sorted-array-ii.java
+++ 80.remove-duplicates-from-sorted-array-ii.java
@@ -5,6 +5,7 @@
  */
 
 // @lc code=start
+// used a hashmap to keep track of the number of times an element has appeared
 class Solution {
     public int removeDuplicates(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
@@ -19,6 +20,29 @@ class Solution {
                 k++;
             }else if(map.containsKey(nums[j]) && map.get(nums[j]) < 2){
                 map.put(nums[j], 2);
+                nums[i] = nums[j];
+                i++;
+                k++;
+            }
+        }
+        return k;
+    }
+}
+
+// used a flag to keep track of the number of times an element has appeared with less space
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int k = 1;
+        int i = 1; // index of insertion
+        int flag = 1; // number of times we have seen it 
+        for(int j = 1; j < nums.length; j++){
+            if(nums[j] != nums[i-1]) { // we find a unique element
+                flag = 1;
+                nums[i] = nums[j];
+                i++;
+                k++;
+            }else if(flag < 2){ // we saw it a second time
+                flag = 2;
                 nums[i] = nums[j];
                 i++;
                 k++;
