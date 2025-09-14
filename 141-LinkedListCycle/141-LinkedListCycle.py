3bf5584a7fe8db04cb26a7874b7dd61babe29be2
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-09-14 12:54:10 -0700

    Create 141-LinkedListCycle

---
+++ 141-LinkedListCycle/141-LinkedListCycle.py
@@ -0,0 +1,16 @@
+# Last updated: 9/14/2025, 12:54:10 PM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast = slow = head
+        if fast is None or fast.next is None: return False
+        while fast is not None and fast.next is not None:
+            fast = fast.next.next
+            slow = slow.next
+            if fast is slow: return True
+        return False
