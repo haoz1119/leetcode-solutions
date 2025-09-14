8f12bd7e11c06297d71692491f0e8112b7e3c08b
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-09-14 12:53:14 -0700

    Create 1666-MakeTheStringGreat

---
+++ 1666-MakeTheStringGreat/1666-MakeTheStringGreat.java
@@ -0,0 +1,28 @@
+// Last updated: 9/14/2025, 12:53:14 PM
+class Solution {
+    public String makeGood(String s) {
+        if(s.length() < 2) return s;
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if(i != s.length() - 1 && s.charAt(i)!=s.charAt(i+1) && Character.toLowerCase(s.charAt(i))==Character.toLowerCase(s.charAt(i+1))){
+                i++;
+            }
+            else {
+                if(!st.empty()){
+                    char c = st.peek();
+                    if(s.charAt(i)!=c && Character.toLowerCase(s.charAt(i))==Character.toLowerCase(c)){
+                        sb.deleteCharAt(sb.length() - 1);
+                        st.pop();
+                        continue;
+                    }
+                }
+                sb.append(s.charAt(i));
+                st.push(s.charAt(i));
+             
+            }
+            
+        }
+        return sb.toString();
+    }
+}
