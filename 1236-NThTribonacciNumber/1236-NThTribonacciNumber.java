9fa3c82ba538ea4f427ea22e9e574b6d52878b50
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-09-14 12:53:17 -0700

    Create 1236-NThTribonacciNumber

---
+++ 1236-NThTribonacciNumber/1236-NThTribonacciNumber.java
@@ -0,0 +1,18 @@
+// Last updated: 9/14/2025, 12:53:16 PM
+class Solution {
+    public int tribonacci(int n) {
+        if (n < 3) {
+            return n > 0 ? 1 : 0;
+        }
+        
+        int a = 0, b = 1, c = 1;
+        for (int i = 0; i < n - 2; ++i) {
+            int tmp = a + b + c;
+            a = b;
+            b = c;
+            c = tmp;
+        }
+        
+        return c;
+    }
+}
