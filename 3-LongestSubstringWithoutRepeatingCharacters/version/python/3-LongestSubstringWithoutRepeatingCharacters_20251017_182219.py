e6e95f6eda292621839d661015c357eeb4490537
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-10-17 18:22:19 -0700

    Create 3-LongestSubstringWithoutRepeatingCharacters

---
+++ 3-LongestSubstringWithoutRepeatingCharacters/version/python/3-LongestSubstringWithoutRepeatingCharacters_20251017_182219.py
@@ -0,0 +1,17 @@
+# Last updated: 10/17/2025, 6:22:19 PM
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        l = 0
+        minl = len(nums)
+        sum = 0
+        found = False
+        for r, n in enumerate(nums):
+            sum+=n
+            while sum >= target:
+                found = True
+                minl = min(minl, r-l+1)
+                sum -= nums[l]
+                l+=1
+                
+        if found:return minl
+        else: return 0
