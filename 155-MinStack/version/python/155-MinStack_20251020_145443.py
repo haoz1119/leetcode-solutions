2ec35f889b364af37e6d302be3255fa14345b1b6
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-10-20 14:54:43 -0700

    Create 155-MinStack

---
+++ 155-MinStack/version/python/155-MinStack_20251020_145443.py
@@ -0,0 +1,16 @@
+# Last updated: 10/20/2025, 2:54:43 PM
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pairs={
+            '(':')',
+            '{':'}',
+            '[':']'
+        }
+        stack = []
+        for ch in s:
+            if ch in pairs:
+                stack.append(ch)
+            else:
+                if not stack or pairs[stack.pop()]!= ch:
+                    return False
+        return not stack
