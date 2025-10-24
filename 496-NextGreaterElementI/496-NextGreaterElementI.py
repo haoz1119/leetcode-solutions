efa1a7095244b692763a81debaac0ce6a720e997
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-10-23 21:14:01 -0700

    File updated at 10/23/2025, 9:14:01 PM

--- 496-NextGreaterElementI/496-NextGreaterElementI.py
+++ 496-NextGreaterElementI/496-NextGreaterElementI.py
@@ -1,19 +1,19 @@
-# Last updated: 10/23/2025, 9:08:58 PM
+# Last updated: 10/23/2025, 9:14:01 PM
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         ans = [-1]*len(nums1)
         minstack = [] # monotonic decreasing
         ng = {} # next greater, value to index pair
-        for i, n in enumerate(nums2):
+        for n in nums2:
             if not minstack:
-                minstack.append(i)
-            elif nums2[minstack[-1]] > n:
-                minstack.append(i)
+                minstack.append(n)
+            elif minstack[-1] > n:
+                minstack.append(n)
             else:
-                while minstack and nums2[minstack[-1]] < n:
+                while minstack and minstack[-1] < n:
                     j = minstack.pop()
-                    ng[nums2[j]] = n
-                minstack.append(i)
+                    ng[j] = n
+                minstack.append(n)
         for i, n in enumerate(nums1):
             if n in ng:
                 ans[i] = ng[n]
