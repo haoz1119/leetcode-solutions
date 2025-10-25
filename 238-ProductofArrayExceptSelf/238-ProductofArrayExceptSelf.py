76ae82c6aa55022c8545cc94ded91f6396235223
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-10-24 21:58:17 -0700

    File updated at 10/24/2025, 9:58:16 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
@@ -1,19 +1,13 @@
-# Last updated: 10/24/2025, 9:47:39 PM
+# Last updated: 10/24/2025, 9:58:16 PM
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prefixp = {}
         p = 1
         ans=[]
-        for i, n in enumerate(nums):
-            prefixp[i] = p
+        for n in nums: # prefix run
+            ans.append(p)
             p *= n
-        suffixp={}
-        p = 1
-        l = len(nums)
-        for i, n in enumerate(nums[::-1]):
-            suffixp[l-1-i] = p
-            p *= n
-            
-        for i in range(len(nums)):
-            ans.append(prefixp[i]*suffixp[i])
+        suffix = 1
+        for i in range(len(nums)-1,-1,-1):
+            ans[i] = ans[i]*suffix
+            suffix*=nums[i]
         return ans
