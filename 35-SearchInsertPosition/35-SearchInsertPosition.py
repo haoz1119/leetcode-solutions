948ea7d751a6d5d80405ce8ef668563b9415fcfa
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-10-25 23:00:05 -0700

    File updated at 10/25/2025, 11:00:05 PM

--- 35-SearchInsertPosition/35-SearchInsertPosition.py
+++ 35-SearchInsertPosition/35-SearchInsertPosition.py
@@ -1,8 +1,8 @@
-# Last updated: 10/25/2025, 10:58:42 PM
+# Last updated: 10/25/2025, 11:00:05 PM
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l,r = 0, len(nums)-1
-        while l <= r: 
+        while l <= r: # when l == r, we can still see if mid=l matches the target
             mid = (l+r)//2
             if nums[mid] > target:
                 r = mid - 1
