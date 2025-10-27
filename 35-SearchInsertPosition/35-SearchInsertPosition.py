47e23efdae30de6095acd242e799cdfe5f1a968c
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-10-26 19:20:46 -0700

    File updated at 10/26/2025, 7:20:46 PM

--- 35-SearchInsertPosition/35-SearchInsertPosition.py
+++ 35-SearchInsertPosition/35-SearchInsertPosition.py
@@ -1,15 +1,20 @@
-# Last updated: 10/26/2025, 7:02:13 PM
+# Last updated: 10/26/2025, 7:20:46 PM
 class Solution:
-    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        n = len(letters)
-        left, right = 0, n - 1
-        ans = n  # “no answer yet” sentinel (= wrap)
-        while left <= right:
-            mid = (left + right) // 2
-            if letters[mid] > target:        # looking for first '>'
-                ans = mid                    # record candidate
-                right = mid - 1              # keep searching left side
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        l, r = 0, len(nums)
+        while l < r:
+            mid = (l+r)//2
+            if nums[mid]>=target:
+                r = mid
             else:
-                left = mid + 1               # need a strictly larger letter
-        return letters[ans] if ans != n else letters[0]
- # inclusive style [l, r] you want to always have l or r off by 1
+                l = mid+1
+        start = l
+        l, r = 0, len(nums)
+        while l < r:
+            mid = (l+r)//2
+            if nums[mid]>target:
+                r = mid
+            else:
+                l = mid+1
+        end = l-1
+        return [start,end] if start != len(nums) and nums[start]==target else [-1,-1]
