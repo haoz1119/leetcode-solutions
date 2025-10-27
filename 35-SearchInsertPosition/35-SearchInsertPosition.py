7041a64d420e75bccfd5f916d20e2a160b7b13b4
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-10-26 18:35:16 -0700

    File updated at 10/26/2025, 6:35:16 PM

--- 35-SearchInsertPosition/35-SearchInsertPosition.py
+++ 35-SearchInsertPosition/35-SearchInsertPosition.py
@@ -1,7 +1,8 @@
-# Last updated: 10/26/2025, 6:30:34 PM
+# Last updated: 10/26/2025, 6:35:16 PM
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
         l, r = 0, len(letters)
+        # mean idea is the unknown window [l, r), the boundary updates keep the window invariant
         while l < r:
             mid = (l+r)//2
             if letters[mid]>target:
