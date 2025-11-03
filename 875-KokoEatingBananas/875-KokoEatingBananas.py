26984d2b0aee80aff37efbe8ac0f42f26d3c9d16
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-11-02 21:38:30 -0800

    File updated at 11/2/2025, 9:38:29 PM

--- 875-KokoEatingBananas/875-KokoEatingBananas.py
+++ 875-KokoEatingBananas/875-KokoEatingBananas.py
@@ -1,10 +1,10 @@
-# Last updated: 11/2/2025, 9:35:08 PM
+# Last updated: 11/2/2025, 9:38:29 PM
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         l, r = 1, max(piles)
         while l < r:
             mid = (l+r)//2
-            hr = sum([math.ceil(x/mid) for x in piles])
+            hr = sum(math.ceil(x/mid) for x in piles)
             if hr > h:
                 l = mid +1
             else:
