00c8ac8decc0a18d7c2b554e794ab83a1bdc1c3c
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-11-03 14:46:52 -0800

    File updated at 11/3/2025, 2:46:52 PM

--- 206-ReverseLinkedList/206-ReverseLinkedList.py
+++ 206-ReverseLinkedList/206-ReverseLinkedList.py
@@ -1,4 +1,4 @@
-# Last updated: 9/14/2025, 12:53:54 PM
+# Last updated: 11/3/2025, 2:46:52 PM
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -6,18 +6,11 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        ## think of it as two pointers with slow/fast pointers, fast is used to get the one after fast and hook the slow pointer as its next
-        if head is None: return head
-        slow = head
-        fast = head.next
-        slow.next = None
-        while fast is not None:
-            temp = fast.next
-            fast.next = slow
-            slow = fast
-            fast = temp
-        
-        return slow
-            
-            
-        
+        prev = None
+        cur = head
+        while cur:
+            next = cur.next
+            cur.next = prev
+            prev = cur
+            cur = next
+        return prev
