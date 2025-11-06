f20c7aa23744acb3cd5a6375550876ecb47cad51
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-11-05 19:50:39 -0800

    File updated at 11/5/2025, 7:50:39 PM

--- 56-MergeIntervals/56-MergeIntervals.py
+++ 56-MergeIntervals/56-MergeIntervals.py
@@ -1,25 +1,18 @@
-# Last updated: 11/5/2025, 6:58:40 PM
+# Last updated: 11/5/2025, 7:50:39 PM
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        def overlap(a1: int, a2: int, b1: int, b2: int)-> bool:
-            if a1<=b2 and a2>=b1:
-                return True
-            return False
-        result=[]
-        if len(intervals) ==1 : return intervals
-        intervals.sort(key=lambda x:(x[0],x[1]))
-        i = 1
-        while i < len(intervals):
-            a1, a2 = intervals[i-1]
-            b1, b2 = intervals[i]
-            i_cp = i
-            while i < len(intervals) and overlap(a1,a2,b1,b2):
-                a1 = min(a1,b1)
-                a2 = max(a2, b2)
-                i+=1
-                if i < len(intervals): b1, b2 = intervals[i]
-            result.append([a1, a2])
-            if i == len(intervals)-1:
-                result.append([b1,b2])
-            i+=1
-        return result
+        if len(intervals) == 1: return intervals
+        intervals.sort(key=lambda x:(x[0]))
+        start = intervals[0][0]
+        end = intervals[0][1]
+        prev = [start, end]
+        merged = []
+        for interval in intervals[1:]:
+            if prev[1] >=interval[0]:
+                prev[1] = max(prev[1], interval[1])
+            else:
+                merged.append(prev)
+                prev = interval
+        merged.append(prev)
+        return merged
+                
