4814e93b922afc6733f57008bb3b3b7c3a7d5cfe
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-11-05 21:33:24 -0800

    File updated at 11/5/2025, 9:33:23 PM

--- 252-MeetingRooms/252-MeetingRooms.py
+++ 252-MeetingRooms/252-MeetingRooms.py
@@ -1,8 +1,8 @@
-# Last updated: 11/5/2025, 9:30:14 PM
+# Last updated: 11/5/2025, 9:33:23 PM
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        intervals.sort(key=lambda x : (x[0]))
         if len(intervals) <= 1: return True
+        intervals.sort(key=lambda x : x[0])
         prev = intervals[0]
         for s, e in intervals[1:]:
             if s < prev[1]:
