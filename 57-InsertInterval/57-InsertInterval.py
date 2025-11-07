bb868446eff5e899e61ea4d9b7a60fb036d40cdf
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-11-06 22:11:37 -0800

    File updated at 11/6/2025, 10:11:36 PM

--- 57-InsertInterval/57-InsertInterval.py
+++ 57-InsertInterval/57-InsertInterval.py
@@ -1,36 +1,19 @@
-# Last updated: 11/6/2025, 9:45:57 PM
+# Last updated: 11/6/2025, 10:11:36 PM
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        def overlap(a1:int, a2:int, b1:int, b2:int)->bool:
-            return a1<=b2 and a2>=b1
-        if not intervals: return [newInterval]
         out = []
-        ns, ne = newInterval
-        found = False
+        if not intervals: return [newInterval]
+        s, e = newInterval
         i = 0
-        if ne < intervals[0][0]:
-            intervals.insert(0, newInterval)
-            return intervals
-        if ns > intervals[-1][1]: 
-            intervals.append(newInterval)
-            return intervals
-        for s, e in intervals:
-            if overlap(s,e,ns,ne) and not found:
-                found = True
-                out.append([min(s,ns), max(e, ne)])
-                i+=1
-                continue
-            if found and out[-1][1]>=s: # we found the overlap and there are more
-                out[-1][1] = max(out[-1][1], e)
-                i+=1
-                continue
-            if found and out[-1][1]< s: # we found the overlap and no overlap anymore so we break
-                out.extend(intervals[i:])
-                break
-            if out and ns>out[-1][1] and ne < s:
-                out.append([ns,ne])
-                out.extend(intervals[i:])
-                break
-            out.append([s,e])
+        n = len(intervals)
+        # append all intervals with no overlap
+        while i < n and s > intervals[i][1]:
+            out.append(intervals[i])
             i+=1
+        while i < n and e >= intervals[i][0]:
+            s = min(intervals[i][0], s)
+            e = max(intervals[i][1], e)
+            i+=1
+        out.append([s, e])
+        out.extend(intervals[i:])
         return out
