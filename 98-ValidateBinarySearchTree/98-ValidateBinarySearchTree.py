0c6fe04b6e8b68e4183b092ce6c2cd06902b2f58
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-11-18 21:51:34 -0800

    File updated at 11/18/2025, 9:51:34 PM

--- 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.py
+++ 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.py
@@ -1,4 +1,4 @@
-# Last updated: 11/18/2025, 9:39:11 PM
+# Last updated: 11/18/2025, 9:51:34 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,19 +6,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        list1 = []
-        def iot(node:Optional[TreeNode]): # in order travelsal and check if prev is always smaller than cur
+    def isValidBST(self, root: TreeNode) -> bool:
+
+        def validate(node, low=-math.inf, high=math.inf):
+            # Empty trees are valid BSTs.
             if not node:
-                return
-            if (not node.left) and (not node.right):
-                list1.append(node.val)
-                return
-            iot(node.left)
-            list1.append(node.val)
-            iot(node.right)
-        iot(root)
-        for i in range(len(list1)-1):
-            if list1[i]>=list1[i+1]:
+                return True
+
+            # The current node's value must be between low and high.
+            if node.val <= low or node.val >= high:
                 return False
-        return True
+
+            # The left and right subtree must also be valid.
+            return validate(
+                node.left, low, node.val
+            ) and validate(node.right, node.val, high) 
+
+        return validate(root)
