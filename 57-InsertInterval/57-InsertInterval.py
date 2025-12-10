0e6ea71313097c5afc1fd2baad0ff5c37ee65f92
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-12-09 17:47:58 -0800

    File updated at 12/9/2025, 5:47:58 PM

--- 57-InsertInterval/57-InsertInterval.py
+++ 57-InsertInterval/57-InsertInterval.py
@@ -1,30 +1,36 @@
-# Last updated: 12/9/2025, 5:32:39 PM
+# Last updated: 12/9/2025, 5:47:58 PM
 1# Definition for a binary tree node.
 2# class TreeNode:
 3#     def __init__(self, val=0, left=None, right=None):
 4#         self.val = val
 5#         self.left = left
 6#         self.right = right
-7class Solution:
-8    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-9        if not root: return []
-10        from collections import deque
-11        queue = deque([root])
-12        lv_idx = 0
-13        result = []
-14        while queue:
-15            lv_size = len(queue)
-16            level = []
-17            for i in range(lv_size):
-18                cur_node = queue.popleft()
-19                if lv_idx % 2:
-20                    level.insert(0, cur_node.val)
-21                else:
-22                    level.append(cur_node.val) 
-23                if cur_node.left:
-24                    queue.append(cur_node.left)
-25                if cur_node.right:
-26                    queue.append(cur_node.right)
-27            result.append(level)  
-28            lv_idx += 1
-29        return result
+7from collections import deque
+8
+9class Solution:
+10    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+11        if not root:
+12            return []
+13        queue = deque([root])
+14        left_to_right = True
+15        result = []
+16
+17        while queue:
+18            level_size = len(queue)
+19            level = deque()
+20            for _ in range(level_size):
+21                node = queue.popleft()
+22                if left_to_right:
+23                    level.append(node.val)
+24                else:
+25                    level.appendleft(node.val)
+26                if node.left:
+27                    queue.append(node.left)
+28                if node.right:
+29                    queue.append(node.right)
+30
+31            result.append(list(level))
+32            left_to_right = not left_to_right
+33
+34        return result
+35
