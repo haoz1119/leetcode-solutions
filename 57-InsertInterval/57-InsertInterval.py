9dcbd827698e38cd73aac5c3ce941881b0f49ae7
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-12-09 16:18:45 -0800

    File updated at 12/9/2025, 4:18:45 PM

--- 57-InsertInterval/57-InsertInterval.py
+++ 57-InsertInterval/57-InsertInterval.py
@@ -1,19 +1,22 @@
-# Last updated: 11/6/2025, 10:11:39 PM
-class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        out = []
-        if not intervals: return [newInterval]
-        s, e = newInterval
-        i = 0
-        n = len(intervals)
-        # append all intervals with no overlap
-        while i < n and s > intervals[i][1]:
-            out.append(intervals[i])
-            i+=1
-        while i < n and e >= intervals[i][0]:
-            s = min(intervals[i][0], s)
-            e = max(intervals[i][1], e)
-            i+=1
-        out.append([s, e])
-        out.extend(intervals[i:])
-        return out
+# Last updated: 12/9/2025, 4:18:45 PM
+1class Solution:
+2    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+3        # skip non overlap intervals
+4        ns, ne = newInterval
+5        merged = []
+6        idx = 0
+7        for s, e in intervals:
+8            if e >= ns:
+9                break
+10            merged.append([s, e])
+11            idx += 1
+12        # merge overlapping intervals
+13        while idx < len(intervals) and intervals[idx][0] <= ne:
+14            min_s = min(ns, intervals[idx][0])
+15            max_e = max(ne, intervals[idx][1])
+16            ns = min_s
+17            ne = max_e
+18            idx += 1
+19        merged.append([ns, ne])
+20        merged.extend(intervals[idx:])
+21        return merged
