921944b094bdc42a0d3b38e1ee249c55d3528e0a
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-12-09 17:21:55 -0800

    File updated at 12/9/2025, 5:21:54 PM

--- 57-InsertInterval/57-InsertInterval.py
+++ 57-InsertInterval/57-InsertInterval.py
@@ -1,22 +1,29 @@
-# Last updated: 12/9/2025, 4:18:45 PM
-1class Solution:
-2    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-3        # skip non overlap intervals
-4        ns, ne = newInterval
-5        merged = []
-6        idx = 0
-7        for s, e in intervals:
-8            if e >= ns:
-9                break
-10            merged.append([s, e])
-11            idx += 1
-12        # merge overlapping intervals
-13        while idx < len(intervals) and intervals[idx][0] <= ne:
-14            min_s = min(ns, intervals[idx][0])
-15            max_e = max(ne, intervals[idx][1])
-16            ns = min_s
-17            ne = max_e
-18            idx += 1
-19        merged.append([ns, ne])
-20        merged.extend(intervals[idx:])
-21        return merged
+# Last updated: 12/9/2025, 5:21:54 PM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+9        if not root: return []
+10        from collections import deque
+11        queue = deque([root])
+12        lv_idx = 0
+13        result = []
+14        while queue:
+15            lv_size = len(queue)
+16            level = []
+17            for i in range(lv_size):
+18                cur_node = queue.popleft()
+19                level.append(cur_node.val) 
+20                if cur_node.left:
+21                    queue.append(cur_node.left)
+22                if cur_node.right:
+23                    queue.append(cur_node.right)
+24
+25            level = list(reversed(level)) if lv_idx % 2 else level
+26            result.append(level)  
+27            lv_idx += 1
+28        return result
