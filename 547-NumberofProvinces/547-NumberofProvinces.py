c79bf8bd9d024682175682d1339d61fc3033951c
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-12-15 14:29:15 -0800

    File updated at 12/15/2025, 2:29:14 PM

--- 547-NumberofProvinces/547-NumberofProvinces.py
+++ 547-NumberofProvinces/547-NumberofProvinces.py
@@ -1,4 +1,4 @@
-# Last updated: 12/15/2025, 2:01:35 PM
+# Last updated: 12/15/2025, 2:29:14 PM
 1class DSU:
 2    def __init__(self, n:int):
 3        self.parent = list(range(n)) # start with one being their own parent
@@ -10,24 +10,20 @@
 9    def union(self, i: int, j: int):
 10        ip, jp = self.find(i), self.find(j)
 11        if self.size[jp] > self.size[ip]: 
-12            self.size[jp] += self.size[ip]
-13            self.parent[ip] =  jp
-14            self.size[ip] = 1
-15        else:
-16            self.size[ip] += self.size[jp]
-17            self.parent[jp] = ip
-18            self.size[jp] = 1
-19
-20
-21class Solution:
-22    def findCircleNum(self, isConnected: List[List[int]]) -> int:
-23        n = len(isConnected)
-24        if n == 1:
-25            return 1
-26        dsu = DSU(n)
-27        for i in range(n):
-28            for j in range(i+1,n,1):
-29                if isConnected[i][j] == 1:
-30                    dsu.union(i, j)
-31        return len({dsu.find(i) for i in range(n)})
-32
+12            ip, jp = jp, ip
+13        self.size[ip] += self.size[jp]
+14        self.parent[jp] = ip
+15
+16
+17class Solution:
+18    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+19        n = len(isConnected)
+20        if n == 1:
+21            return 1
+22        dsu = DSU(n)
+23        for i in range(n):
+24            for j in range(i+1,n,1):
+25                if isConnected[i][j] == 1:
+26                    dsu.union(i, j)
+27        return len({dsu.find(i) for i in range(n)})
+28
