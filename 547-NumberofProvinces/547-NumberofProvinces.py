a357e06db216de9a76ebf8af87f534f0fc198e5c
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-12-15 15:24:44 -0800

    File updated at 12/15/2025, 3:24:44 PM

--- 547-NumberofProvinces/547-NumberofProvinces.py
+++ 547-NumberofProvinces/547-NumberofProvinces.py
@@ -1,31 +1,28 @@
-# Last updated: 12/15/2025, 2:30:44 PM
+# Last updated: 12/15/2025, 3:24:44 PM
 1class DSU:
-2    def __init__(self, n:int):
-3        self.parent = list(range(n)) # start with one being their own parent
-4        self.size = [1] * n
-5    def find(self, idx: int) -> int:
-6        if idx != self.parent[idx]:
-7            self.parent[idx] = self.find(self.parent[idx]) # parent path compression 
-8        return self.parent[idx]
-9    def union(self, i: int, j: int):
-10        ip, jp = self.find(i), self.find(j)
-11        if ip == jp:
-12            return
-13        if self.size[jp] > self.size[ip]: 
-14            ip, jp = jp, ip
-15        self.size[ip] += self.size[jp]
-16        self.parent[jp] = ip
-17
-18
+2    def __init__(self, n: int):
+3        self.parent = list(range(n))
+4        self.size = [1]*n
+5    def find(self, i: int):
+6        if self.parent[i] != i:
+7            self.parent[i] = self.find(self.parent[i])
+8        return self.parent[i]
+9
+10    def union(self, i: int, j: int)->bool:
+11        ir, jr = self.find(i), self.find(j)
+12        if ir == jr:
+13            return True
+14        if self.size[ir] > self.size[jr]:
+15            ir, jr = jr, ir
+16        self.parent[ir] = jr
+17        self.size[jr] += self.size[ir]
+18        return False
 19class Solution:
-20    def findCircleNum(self, isConnected: List[List[int]]) -> int:
-21        n = len(isConnected)
-22        if n == 1:
-23            return 1
-24        dsu = DSU(n)
-25        for i in range(n):
-26            for j in range(i+1,n,1):
-27                if isConnected[i][j] == 1:
-28                    dsu.union(i, j)
-29        return len({dsu.find(i) for i in range(n)})
-30
+20    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+21        n = len(edges)
+22        dsu = DSU(n+1)
+23        for i, j in edges:
+24            if dsu.union(i, j):
+25                return [i, j]
+26            
+27        return []
