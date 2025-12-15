e8f67aa01161408a833fd81a06b4eb4b022bde9b
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-12-14 18:46:24 -0800

    File updated at 12/14/2025, 6:46:24 PM

--- 207-CourseSchedule/207-CourseSchedule.py
+++ 207-CourseSchedule/207-CourseSchedule.py
@@ -1,29 +1,28 @@
-# Last updated: 12/14/2025, 5:57:46 PM
+# Last updated: 12/14/2025, 6:46:24 PM
 1from collections import deque
 2class Solution:
-3    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-4        if numCourses == 1 or len(prerequisites) == 0: return True
-5        adj = {}
-6        indegree = [0]*numCourses
-7        # build adj and indegree
-8        for a, b in prerequisites:
-9            if b not in adj:
-10                adj[b] = []
-11            adj[b].append(a)
-12            indegree[a] += 1
-13        # kahn's algorithm
-14        free_class = [key for key, v in enumerate(indegree) if v == 0]
-15        taken = len(free_class)
-16        queue = deque(free_class)
-17        while queue:
-18            pre = queue.popleft()
-19            if pre in adj:
-20                for nei in adj[pre]:
-21                    indegree[nei] -= 1
-22                    if indegree[nei] == 0:
-23                        queue.append(nei)
-24                        taken += 1
-25        return taken == numCourses
-26        
+3    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+4        if numCourses == 1:
+5            return [0]
+6        if len(prerequisites) == 0:
+7            return list(range(numCourses))
+8        # build adj and indegree
+9        adj = [[] for _ in range(numCourses)]
+10        indegree = [0]*numCourses
+11        for a, b in prerequisites:
+12            adj[b].append(a)
+13            indegree[a]+=1
+14        # kahn algo
+15        free_class = [i for i, v in enumerate(indegree) if v == 0]
+16        order = free_class[:] # shallow copy??
+17        queue = deque(free_class)
+18        while queue:
+19            pre = queue.popleft()
+20            for nei in adj[pre]:
+21                indegree[nei] -= 1
+22                if indegree[nei] == 0:
+23                    queue.append(nei)
+24                    order.append(nei)
+25
+26        return order if len(order) == numCourses else []
 27
-28
