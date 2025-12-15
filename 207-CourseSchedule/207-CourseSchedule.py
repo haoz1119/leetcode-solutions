5fb2dbc93e88238c0214fc85c2bea66d684558e6
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-12-14 17:53:28 -0800

    File updated at 12/14/2025, 5:53:28 PM

--- 207-CourseSchedule/207-CourseSchedule.py
+++ 207-CourseSchedule/207-CourseSchedule.py
@@ -1,4 +1,4 @@
-# Last updated: 12/14/2025, 5:39:06 PM
+# Last updated: 12/14/2025, 5:53:28 PM
 1from collections import deque
 2class Solution:
 3    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
@@ -17,17 +17,15 @@
 16                indegree[b] = 0
 17        # kahn's algorithm
 18        free_class = [key for key, v in indegree.items() if v == 0]
-19        taken = len(free_class)
-20        queue = deque(free_class)
-21        while queue:
-22            pre = queue.popleft()
-23            if pre in adj:
-24                for nei in adj[pre]:
-25                    indegree[nei] -= 1
-26                    if indegree[nei] == 0:
-27                        queue.append(nei)
-28                        taken += 1
-29        return True if sum(indegree.values()) == 0 else False
-30        
-31
-32
+19        queue = deque(free_class)
+20        while queue:
+21            pre = queue.popleft()
+22            if pre in adj:
+23                for nei in adj[pre]:
+24                    indegree[nei] -= 1
+25                    if indegree[nei] == 0:
+26                        queue.append(nei)
+27        return True if sum(indegree.values()) == 0 else False
+28        
+29
+30
