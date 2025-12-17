dccea88c89f0f6f59d5a60d23dc1459aa9ce294a
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-12-17 15:38:30 -0800

    File updated at 12/17/2025, 3:38:30 PM

--- 78-Subsets/78-Subsets.py
+++ 78-Subsets/78-Subsets.py
@@ -1,4 +1,4 @@
-# Last updated: 12/17/2025, 3:12:40 PM
+# Last updated: 12/17/2025, 3:38:30 PM
 1class Solution:
 2    def combine(self, n: int, k: int) -> List[List[int]]:
 3        path = []
@@ -8,10 +8,11 @@
 7            if len(path) == k:
 8                res.append(path[:])
 9                return
-10            for i in range(start, n):
-11                path.append(i+1)
-12                backtrack(i+1)
-13                path.pop()
-14
-15        backtrack(0)  
-16        return res
+10            remaining_slots = k - len(path)
+11            for i in range(start, n - remaining_slots+1):
+12                path.append(i+1)
+13                backtrack(i+1)
+14                path.pop()
+15
+16        backtrack(0)  
+17        return res
