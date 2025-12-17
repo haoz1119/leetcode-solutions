5eb6a5992bfb9ebe6d377d9f509b7b211aa5cda9
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-12-17 15:12:40 -0800

    File updated at 12/17/2025, 3:12:40 PM

--- 78-Subsets/78-Subsets.py
+++ 78-Subsets/78-Subsets.py
@@ -1,14 +1,17 @@
-# Last updated: 12/17/2025, 3:00:39 PM
+# Last updated: 12/17/2025, 3:12:40 PM
 1class Solution:
-2    def subsets(self, nums: List[int]) -> List[List[int]]:
+2    def combine(self, n: int, k: int) -> List[List[int]]:
 3        path = []
 4        res = []
-5        n = len(nums)
-6        def backtrack(idx: int):
-7            res.append(path.copy())
-8            for i in range(idx, n):
-9                path.append(nums[i])   # choose nums[i] as the next element
-10                backtrack(i + 1)       # now choose further elements only from i+1 onward
-11                path.pop()             # unchoose nums[i], so we can try the next i
-12        backtrack(0)
-13        return res
+5
+6        def backtrack(start: int)-> None:
+7            if len(path) == k:
+8                res.append(path[:])
+9                return
+10            for i in range(start, n):
+11                path.append(i+1)
+12                backtrack(i+1)
+13                path.pop()
+14
+15        backtrack(0)  
+16        return res
