d4feedcb84b9da6206b8512f07fc1897a458234b
ItsHao <68983578+haoz1119@users.noreply.github.com>
2025-12-17 15:00:39 -0800

    File updated at 12/17/2025, 3:00:39 PM

--- 78-Subsets/78-Subsets.py
+++ 78-Subsets/78-Subsets.py
@@ -1,4 +1,4 @@
-# Last updated: 12/17/2025, 2:44:53 PM
+# Last updated: 12/17/2025, 3:00:39 PM
 1class Solution:
 2    def subsets(self, nums: List[int]) -> List[List[int]]:
 3        path = []
@@ -6,9 +6,9 @@
 5        n = len(nums)
 6        def backtrack(idx: int):
 7            res.append(path.copy())
-8            for i in range(idx, n): 
-9                path.append(nums[i])
-10                backtrack(i+1)
-11                path.pop()
+8            for i in range(idx, n):
+9                path.append(nums[i])   # choose nums[i] as the next element
+10                backtrack(i + 1)       # now choose further elements only from i+1 onward
+11                path.pop()             # unchoose nums[i], so we can try the next i
 12        backtrack(0)
 13        return res
