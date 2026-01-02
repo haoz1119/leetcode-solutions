da8ccdc23096afce6cb4699711706de55eaaa131
ItsHao <68983578+haoz1119@users.noreply.github.com>
2026-01-01 16:39:20 -0800

    File updated at 1/1/2026, 4:39:19 PM

--- 322-CoinChange/322-CoinChange.py
+++ 322-CoinChange/322-CoinChange.py
@@ -1,11 +1,10 @@
-# Last updated: 1/1/2026, 3:42:56 PM
+# Last updated: 1/1/2026, 4:39:19 PM
 1class Solution:
-2    def coinChange(self, coins: List[int], amount: int) -> int:
-3        if amount == 0: return 0
-4        dp = [float('inf')]*(amount+1)
-5        dp[0] = 0
-6        for coin in coins:
-7            for i in range(coin, amount+1):
-8                dp[i] = min(dp[i], 1+dp[i - coin])
-9
-10        return dp[amount] if dp[amount] != float('inf') else -1
+2    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+3        n = len(s)
+4        dp = [False] * (n+1)
+5        dp[0] = True
+6        for i in range(1,n+1):
+7            dp[i] = any(dp[j] and s[j:i] in wordDict for j in range(i))
+8                
+9        return dp[n]
