875a76662d54fc1ec2cdcfc92188f51dd8bf1a95
ItsHao <68983578+haoz1119@users.noreply.github.com>
2026-01-01 16:46:10 -0800

    File updated at 1/1/2026, 4:46:10 PM

--- 322-CoinChange/322-CoinChange.py
+++ 322-CoinChange/322-CoinChange.py
@@ -1,10 +1,11 @@
-# Last updated: 1/1/2026, 4:43:54 PM
+# Last updated: 1/1/2026, 4:46:10 PM
 1class Solution:
 2    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
 3        n = len(s)
-4        dp = [False] * (n+1)
-5        dp[0] = True
-6        for i in range(1,n+1):
-7            dp[i] = any(dp[j] and s[j:i] in wordDict for j in range(i))
-8                
-9        return dp[n]
+4        words = set(wordDict)
+5        dp = [False] * (n+1)
+6        dp[0] = True
+7        for i in range(1,n+1):
+8            dp[i] = any(dp[j] and s[j:i] in words for j in range(i))
+9                
+10        return dp[n]
