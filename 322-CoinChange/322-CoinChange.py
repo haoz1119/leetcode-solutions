c3235a7773ed919567630194522ef4895d2690ca
ItsHao <68983578+haoz1119@users.noreply.github.com>
2026-01-02 22:08:49 -0800

    File updated at 1/2/2026, 10:08:49 PM

--- 322-CoinChange/322-CoinChange.py
+++ 322-CoinChange/322-CoinChange.py
@@ -1,4 +1,4 @@
-# Last updated: 1/2/2026, 9:56:23 PM
+# Last updated: 1/2/2026, 10:08:49 PM
 1from functools import lru_cache
 2class Solution:
 3    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
@@ -8,18 +8,16 @@
 7        max_len = max(len(w) for w in words)
 8        @lru_cache(None)
 9        def dfs(start, end):
-10            if end - start < min_len : return False
-11            if end - start > max_len : return False
-12            if s[start:end] not in words:
-13                return False
-14            if end == n:
-15                return True
-16            for i in range(end+1, n+1):
-17                if dfs(end, i):
-18                    return True
-19            return False
-20                
-21        for i in range(1, n+1):
-22            if dfs(0, i):
-23                return True
-24        return False
+10            if s[start:end] not in words:
+11                return False
+12            if end == n:
+13                return True
+14            for i in range(end+min_len, min(n, end+max_len)+1):
+15                if dfs(end, i):
+16                    return True
+17            return False
+18                
+19        for i in range(1, n+1):
+20            if dfs(0, i):
+21                return True
+22        return False
