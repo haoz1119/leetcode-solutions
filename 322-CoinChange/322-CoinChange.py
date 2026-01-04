631c49da86afa438208aa524cb1e61f5f26259e3
ItsHao <68983578+haoz1119@users.noreply.github.com>
2026-01-03 17:07:33 -0800

    File updated at 1/3/2026, 5:07:33 PM

--- 322-CoinChange/322-CoinChange.py
+++ 322-CoinChange/322-CoinChange.py
@@ -1,23 +1,31 @@
-# Last updated: 1/2/2026, 10:08:49 PM
-1from functools import lru_cache
-2class Solution:
-3    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-4        n = len(s)
-5        words = set(wordDict)
-6        min_len = min(len(w) for w in words)
-7        max_len = max(len(w) for w in words)
-8        @lru_cache(None)
-9        def dfs(start, end):
-10            if s[start:end] not in words:
+# Last updated: 1/3/2026, 5:07:33 PM
+1class Solution:
+2    def exist(self, board: List[List[str]], word: str) -> bool:
+3        m = len(board)
+4        n = len(board[0])
+5        l = len(word)
+6        dirs = [(1,0),(-1,0),(0,1),(0,-1)]
+7        def inbound(r, c):
+8            return 0<=r<m and 0<=c<n
+9        def dfs(r, c, i):
+10            if board[r][c] != word[i]:
 11                return False
-12            if end == n:
+12            if i == l-1:
 13                return True
-14            for i in range(end+min_len, min(n, end+max_len)+1):
-15                if dfs(end, i):
-16                    return True
-17            return False
-18                
-19        for i in range(1, n+1):
-20            if dfs(0, i):
-21                return True
-22        return False
+14            tmp = board[r][c]
+15            board[r][c] = '#'
+16            for dr, dc in dirs:
+17                nr, nc = r+dr, c+dc
+18                if inbound(nr, nc) and board[nr][nc] != '#':
+19                    if dfs(nr, nc, i+1):
+20                        board[r][c] = tmp
+21                        return True
+22            board[r][c] = tmp
+23            return False
+24        for r in range(m):
+25            for c in range(n):
+26                if dfs(r, c, 0):
+27                    return True
+28        return False
+29
+30
