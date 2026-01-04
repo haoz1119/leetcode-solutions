ffe5918f55dded4b5a9646927a3ba4c3d71184a4
ItsHao <68983578+haoz1119@users.noreply.github.com>
2026-01-03 17:10:44 -0800

    File updated at 1/3/2026, 5:10:43 PM

--- 322-CoinChange/322-CoinChange.py
+++ 322-CoinChange/322-CoinChange.py
@@ -1,31 +1,28 @@
-# Last updated: 1/3/2026, 5:07:33 PM
+# Last updated: 1/3/2026, 5:10:43 PM
 1class Solution:
 2    def exist(self, board: List[List[str]], word: str) -> bool:
-3        m = len(board)
-4        n = len(board[0])
-5        l = len(word)
+3        l = len(word)
+4        m = len(board)
+5        n = len(board[0])
 6        dirs = [(1,0),(-1,0),(0,1),(0,-1)]
 7        def inbound(r, c):
 8            return 0<=r<m and 0<=c<n
 9        def dfs(r, c, i):
-10            if board[r][c] != word[i]:
-11                return False
-12            if i == l-1:
-13                return True
-14            tmp = board[r][c]
-15            board[r][c] = '#'
-16            for dr, dc in dirs:
-17                nr, nc = r+dr, c+dc
-18                if inbound(nr, nc) and board[nr][nc] != '#':
-19                    if dfs(nr, nc, i+1):
-20                        board[r][c] = tmp
-21                        return True
-22            board[r][c] = tmp
-23            return False
-24        for r in range(m):
-25            for c in range(n):
-26                if dfs(r, c, 0):
-27                    return True
-28        return False
-29
-30
+10            if i == l:
+11                return True
+12            for dr, dc in dirs:
+13                nr, nc = r+dr, c+dc
+14                if inbound(nr, nc) and board[nr][nc] == word[i]:
+15                    board[nr][nc] = '#'
+16                    if dfs(nr, nc, i+1):
+17                        return True
+18                    board[nr][nc] = word[i]
+19            return False
+20        for r in range(m):
+21            for c in range(n):
+22                if board[r][c] == word[0]:
+23                    board[r][c] = '#'
+24                    if dfs(r, c, 1):
+25                        return True
+26                    board[r][c] = word[0]
+27        return False
