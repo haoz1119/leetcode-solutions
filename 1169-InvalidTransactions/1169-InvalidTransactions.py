fd51ac4b6e67e18f9f9cf562fe67c59dad8e1cff
ItsHao <68983578+haoz1119@users.noreply.github.com>
2026-01-07 16:47:45 -0800

    File updated at 1/7/2026, 4:47:44 PM

--- 1169-InvalidTransactions/1169-InvalidTransactions.py
+++ 1169-InvalidTransactions/1169-InvalidTransactions.py
@@ -1,28 +1,18 @@
-# Last updated: 1/7/2026, 4:20:15 PM
-1from collections import defaultdict
-2class Solution:
-3    def invalidTransactions(self, transactions: List[str]) -> List[str]:
-4        trnx = []
-5        result = []
-6        for x in transactions:
-7            tx = x.split(",")
-8            trnx.append((tx[0], int(tx[1]), int(tx[2]), tx[3]))
-9        trnx_dict = defaultdict(list)
-10        for name, t, amount, city in trnx:
-11            trnx_dict[name].append([t, amount, city])
-12        
-13        for name, value in trnx_dict.items():
-14            l = len(value)
-15            for i in range(l):
-16                if value[i][1] > 1000 and len(value[i]) <= 3:
-17                    value[i].append(0)
-18                for j in range(i+1, l):
-19                    if (abs(value[i][0] - value[j][0]) <= 60 and value[i][2] != value[j][2]):
-20                        if len(value[i]) <= 3: value[i].append(0)
-21                        if len(value[j]) <= 3: value[j].append(0)
-22        for name, value in trnx_dict.items():
-23            for x in value:
-24                if len(x) == 4:
-25                    result.append(f"{name},{x[0]},{x[1]},{x[2]}")
-26        return result
-27
+# Last updated: 1/7/2026, 4:47:44 PM
+1class Solution:
+2    def invalidTransactions(self, transactions: List[str]) -> List[str]:
+3        invalid = []
+4        
+5        for i, t1 in enumerate(transactions):
+6            name1, time1, amount1, city1 = t1.split(',')
+7            if int(amount1) > 1000:
+8                invalid.append(t1)
+9                continue
+10            for j, t2 in enumerate(transactions):
+11                if i != j: 
+12                    name2, time2, amount2, city2 = t2.split(',')
+13                    if name1 == name2 and city1 != city2 and abs(int(time1) - int(time2)) <= 60:
+14                        invalid.append(t1)
+15                        break
+16        
+17        return invalid
