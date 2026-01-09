6b472944fd8ea7d584fcff735c789e8cc37b0af0
ItsHao <68983578+haoz1119@users.noreply.github.com>
2026-01-08 22:27:39 -0800

    File updated at 1/8/2026, 10:27:39 PM

--- 399-EvaluateDivision/399-EvaluateDivision.py
+++ 399-EvaluateDivision/399-EvaluateDivision.py
@@ -1,4 +1,4 @@
-# Last updated: 1/8/2026, 9:15:03 PM
+# Last updated: 1/8/2026, 10:27:39 PM
 1from collections import defaultdict, deque
 2class Solution:
 3    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
@@ -11,27 +11,27 @@
 10            adj[b].append((a, 1/values[i]))
 11        # bfs to find for each query
 12        res = []
-13        def bfs(a, c):
+13        def dfs(a, c, visited):
 14            if a not in adj or c not in adj:
 15                return -1.0
 16            if a == c:
 17                return 1.0
-18            queue = deque([(a, 1)])
-19            visited = set()
-20            visited.add(a)
-21            while queue:
-22                node, rcost = queue.popleft()
-23                for nei, cost in adj[node]:
-24                    if nei not in visited:
-25                        visited.add(nei)
-26                        if nei == c:
-27                            rcost *= cost
-28                            return rcost
-29                        else:
-30                            queue.append((nei, rcost*cost))
-31            return -1.0
-32        for a, c in queries:
-33            res.append(bfs(a, c))
+18            rcost = 1
+19            visited.add(a)
+20            for nei, cost in adj[a]:
+21                if nei not in visited:
+22                    visited.add(nei)
+23                    if nei == c:
+24                        return rcost*cost
+25                    else:
+26                        result = dfs(nei, c, visited)
+27                        if result != -1.0:
+28                            return rcost*cost*result
+29            return -1.0
+30                    
+31        for a, c in queries:
+32            visited = set()
+33            res.append(dfs(a, c, visited))
 34        return res
 35            
 36
