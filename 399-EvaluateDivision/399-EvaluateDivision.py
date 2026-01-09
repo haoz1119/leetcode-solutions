76bf8b2222314411eebf6e80d33c32d02cb7367e
ItsHao <68983578+haoz1119@users.noreply.github.com>
2026-01-08 22:29:42 -0800

    File updated at 1/8/2026, 10:29:42 PM

--- 399-EvaluateDivision/399-EvaluateDivision.py
+++ 399-EvaluateDivision/399-EvaluateDivision.py
@@ -1,4 +1,4 @@
-# Last updated: 1/8/2026, 10:27:39 PM
+# Last updated: 1/8/2026, 10:29:42 PM
 1from collections import defaultdict, deque
 2class Solution:
 3    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
@@ -20,18 +20,17 @@
 19            visited.add(a)
 20            for nei, cost in adj[a]:
 21                if nei not in visited:
-22                    visited.add(nei)
-23                    if nei == c:
-24                        return rcost*cost
-25                    else:
-26                        result = dfs(nei, c, visited)
-27                        if result != -1.0:
-28                            return rcost*cost*result
-29            return -1.0
-30                    
-31        for a, c in queries:
-32            visited = set()
-33            res.append(dfs(a, c, visited))
-34        return res
-35            
-36
+22                    if nei == c:
+23                        return rcost*cost
+24                    else:
+25                        result = dfs(nei, c, visited)
+26                        if result != -1.0:
+27                            return rcost*cost*result
+28            return -1.0
+29                    
+30        for a, c in queries:
+31            visited = set()
+32            res.append(dfs(a, c, visited))
+33        return res
+34            
+35
