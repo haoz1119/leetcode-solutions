7356280aebb4f8adf39a305154d7f27c35b655c0
ItsHao <68983578+haoz1119@users.noreply.github.com>
2026-01-10 00:34:06 -0800

    File updated at 1/10/2026, 12:34:06 AM

--- 146-LRUCache/146-LRUCache.py
+++ 146-LRUCache/146-LRUCache.py
@@ -1,8 +1,11 @@
-# Last updated: 1/10/2026, 12:06:25 AM
+# Last updated: 1/10/2026, 12:34:06 AM
 1class Solution:
-2    def findTheWinner(self, n: int, k: int) -> int:
-3        def helper(i):
-4            if i == 1:
-5                return 0
-6            return (helper(i-1)+k)%i
-7        return helper(n)+1
+2    def topKFrequent(self, arr: List[int], k: int) -> List[int]:
+3        count = Counter(arr)
+4        hp = []
+5        for key, value in count.items():
+6            heapq.heappush(hp, (value, key))
+7            if len(hp) > k:
+8                heapq.heappop(hp) 
+9        return [key for _, key in hp]
+10
