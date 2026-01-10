e6ad13028808d9cb4eb2cd552288f245e07d1af8
ItsHao <68983578+haoz1119@users.noreply.github.com>
2026-01-09 16:32:12 -0800

    File updated at 1/9/2026, 4:32:12 PM

--- 399-EvaluateDivision/399-EvaluateDivision.py
+++ 399-EvaluateDivision/399-EvaluateDivision.py
@@ -1,35 +1,32 @@
-# Last updated: 1/8/2026, 10:33:57 PM
+# Last updated: 1/9/2026, 4:32:12 PM
 1from collections import defaultdict, deque
 2class Solution:
-3    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-4        # build adj list
-5        n = len(equations)
-6        adj = defaultdict(list)
-7        for i in range(n):
-8            a, b = equations[i]
+3    def calcEquation(
+4        self, equations: List[List[str]], values: List[float], queries: List[List[str]]
+5    ) -> List[float]:
+6        # Build adjacency list
+7        adj = defaultdict(list)
+8        for i, (a, b) in enumerate(equations):
 9            adj[a].append((b, values[i]))
-10            adj[b].append((a, 1/values[i]))
-11        # bfs to find for each query
+10            adj[b].append((a, 1 / values[i]))
+11
 12        res = []
-13        def dfs(a, c):
-14            if a not in adj or c not in adj:
-15                return -1.0
-16            if a == c:
-17                return 1.0
-18            visited.add(a)
-19            for nei, cost in adj[a]:
-20                if nei not in visited:
-21                    if nei == c:
-22                        return cost
-23                    else:
-24                        result = dfs(nei, c)
-25                        if result != -1.0:
-26                            return cost*result
-27            return -1.0
-28                    
-29        for a, c in queries:
-30            visited = set()
-31            res.append(dfs(a, c))
-32        return res
-33            
-34
+13
+14        def dfs(a: str, c: str) -> float:
+15            if a not in adj or c not in adj:
+16                return -1.0
+17            if a == c:
+18                return 1.0
+19            visited.add(a)
+20            for nei, cost in adj[a]:
+21                if nei not in visited:
+22                    result = dfs(nei, c)
+23                    if result != -1.0:
+24                        return cost * result
+25            return -1.0
+26
+27        for a, c in queries:
+28            visited = set()  # Reset for each query
+29            res.append(dfs(a, c))
+30        return res
+31
