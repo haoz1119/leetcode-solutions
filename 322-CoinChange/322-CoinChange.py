18e1e459642921e16bd658eeeda8c9f8217a36cf
ItsHao <68983578+haoz1119@users.noreply.github.com>
2026-01-12 23:09:58 -0800

    File updated at 1/12/2026, 11:09:58 PM

--- 322-CoinChange/322-CoinChange.py
+++ 322-CoinChange/322-CoinChange.py
@@ -1,32 +1,9 @@
-# Last updated: 1/3/2026, 6:13:46 PM
-1from collections import deque
-2class Solution:
-3    def numIslands(self, grid: List[List[str]]) -> int:
-4        dirs = [(1,0),(-1,0),(0,1),(0,-1)]
-5        def inbound(r, c, m, n):
-6            return (r >= 0 and r < m) and (c >= 0 and c < n)
-7        def bfs():
-8            while queue:
-9                i, j = queue.popleft()
-10                for dr, dc in dirs:
-11                    nr, nc = i+dr, j+ dc
-12                    if inbound(nr, nc, m, n) and grid[nr][nc] == '1' and (nr, nc) not in visited:
-13                        visited.add((nr, nc))
-14                        queue.append((nr, nc))
-15
-16        m = len(grid)
-17        n = len(grid[0])
-18        visited = set()
-19        queue = deque()
-20        count = 0
-21        for i in range(m):
-22            for j in range(n):
-23                if grid[i][j] == '1' and (i, j) not in visited:
-24                    count += 1
-25                    visited.add((i, j))
-26                    queue.append((i,j))
-27                    bfs()
-28        return count
-29
-30
-31
+# Last updated: 1/12/2026, 11:09:58 PM
+1class Solution:
+2    def coinChange(self, coins: List[int], amount: int) -> int:
+3        dp = [math.inf]*(amount+1)
+4        dp[0]=0
+5        for coin in coins:
+6            for i in range(coin,amount+1):
+7                dp[i] = min(dp[i], dp[i-coin]+1)
+8        return dp[amount] if dp[amount] != math.inf else -1
