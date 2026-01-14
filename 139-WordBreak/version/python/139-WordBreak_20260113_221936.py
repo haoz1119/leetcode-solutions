bb0a2e149b92021e40cbb01498d8cb7eee735f23
ItsHao <68983578+haoz1119@users.noreply.github.com>
2026-01-13 22:19:37 -0800

    Create 139-WordBreak

---
+++ 139-WordBreak/version/python/139-WordBreak_20260113_221936.py
@@ -0,0 +1,15 @@
+# Last updated: 1/13/2026, 10:19:36 PM
+1class Solution:
+2    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+3        @cache
+4        def dp(i):
+5            if i < 0:
+6                return True
+7
+8            for word in wordDict:
+9                if s[i - len(word) + 1 : i + 1] == word and dp(i - len(word)):
+10                    return True
+11
+12            return False
+13
+14        return dp(len(s) - 1)
