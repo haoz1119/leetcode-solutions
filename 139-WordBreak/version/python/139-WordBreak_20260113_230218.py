c640e5e2fd55d8cace9e9535564813ae24904c78
ItsHao <68983578+haoz1119@users.noreply.github.com>
2026-01-13 23:02:18 -0800

    Create 139-WordBreak

---
+++ 139-WordBreak/version/python/139-WordBreak_20260113_230218.py
@@ -0,0 +1,13 @@
+# Last updated: 1/13/2026, 11:02:18 PM
+1class Solution:
+2    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+3        n = len(s)
+4        words = set(wordDict)
+5        max_len = max(len(w) for w in words)
+6        dp = [False] * (n+1)
+7        dp[0] = True
+8        for i in range(1,n+1):
+9            start = max(0, i-max_len)
+10            dp[i] = any(dp[j] and s[j:i] in words for j in range(start, i))
+11                
+12        return dp[n]
