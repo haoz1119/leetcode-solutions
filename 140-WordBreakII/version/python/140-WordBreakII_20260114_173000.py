ea4018eba732fc57163d319fe1f5428cf3b7589e
ItsHao <68983578+haoz1119@users.noreply.github.com>
2026-01-14 17:30:01 -0800

    Create 140-WordBreakII

---
+++ 140-WordBreakII/version/python/140-WordBreakII_20260114_173000.py
@@ -0,0 +1,6 @@
+# Last updated: 1/14/2026, 5:30:00 PM
+text = "   hello world   "
+
+text.strip()   # "hello world"      (removes both ends)
+text.lstrip()  # "hello world   "   (removes left/leading only)
+text.rstrip()  # "   hello world"   (removes right/trailing only)
