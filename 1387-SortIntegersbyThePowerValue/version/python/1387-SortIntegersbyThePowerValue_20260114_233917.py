23ab7b5630540d32a90481367091053953aab62f
ItsHao <68983578+haoz1119@users.noreply.github.com>
2026-01-14 23:39:17 -0800

    Create 1387-SortIntegersbyThePowerValue

---
+++ 1387-SortIntegersbyThePowerValue/version/python/1387-SortIntegersbyThePowerValue_20260114_233917.py
@@ -0,0 +1,17 @@
+# Last updated: 1/14/2026, 11:39:17 PM
+1from functools import lru_cache
+2class Solution:
+3    def getKth(self, lo: int, hi: int, k: int) -> int:
+4        dic = {}
+5        @lru_cache(None)
+6        def dfs(n):
+7            if n == 1:
+8                return 0
+9            if n % 2 == 0:
+10                return 1 + dfs(n//2)
+11            else:
+12                return 1 + dfs(3*n+1)
+13        for n in range(lo, hi+1):
+14            dic[n] = dfs(n)
+15        return sorted(dic.keys(), key=lambda x: (dic[x], x))[k-1]
+16
